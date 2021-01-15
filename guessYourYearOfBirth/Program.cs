241ad0d9ea0084b48f40ef5cbfdb8855131a16c9
Hawshad Hawre <hawshad.hamakarim@gmail.com>
2021-01-15 15:09:15 +0300

    Adding my files

---
+++ guessYourYearOfBirth/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace guessYourYearOfBirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert Your Age");
+            String myAge = Console.ReadLine();
+            int age = int.Parse(myAge);
+
+            Console.WriteLine("Insert the Year");
+            String year = Console.ReadLine();
+            int thisYear = int.Parse(year);
+
+            Console.WriteLine("Your year of birth is : "+ (thisYear - age));
+        }
+    }
+}
